8ac8fb34efd6daf75a0b5ad3524f5d76f9dd0152
hadesshark <hades_shark@hotmail.com>
2017-05-03 21:57:07 +0800

    增加基本資料有點難表達成程式碼

--- function_tests.py
+++ function_tests.py
@@ -1,12 +1,33 @@
-# 把想要抓取的小說，給於基本資料
+# -*- coding: utf-8 -*-
+import requests
+from lxml import etree
+import json
+import sys
 
 
-
-# 傳送請求，存取小說在指定的位置
-    # 傳送請求，獲得需要的小說頁面
-
-
-    # 內容需要修改的部分進行處理
+import downloader
+import unittest
 
 
-    # 把獲得的小說內容存在指定的位置
+class TestResponse(unittest.TestCase):
+
+    # 把想要抓取的小說，給於基本資料
+    def setUp(self):
+        self.response = downloader.response
+
+
+    # 傳送請求，存取小說在指定的位置
+        # 傳送請求，獲得需要的小說
+
+
+        # 內容需要修改的部分進行處理
+
+
+        # 把獲得的小說內容存在指定的位置
+
+    def test_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
