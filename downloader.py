34cbc597338f52049d59d860bc4a95e169808929
hadesshark <hades_shark@hotmail.com>
2017-05-03 14:27:05 +0800

    增加 downloader 和 unittest

---
+++ downloader.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+import requests
+from lxml import etree
+import json
+import sys
---
+++ unittest.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import requests
+from lxml import etree
+import json
+import sys
+
+import unittest
