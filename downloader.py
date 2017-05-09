24510b680e2f432b2333e60c846fae2de482d01e
hadesshark <hades_shark@hotmail.com>
2017-05-09 19:56:08 +0800

    函數式寫法好像很難測試

--- downloader.py
+++ downloader.py
@@ -1,28 +1,25 @@
 # -*- coding: utf-8 -*-
 import requests
-from lxml import etree
-import json
-import sys
-
 from bs4 import BeautifulSoup
 
 
 url = "https://ck101.com/thread-3845788-1-1.html"
 
-class Response(object):
-
-    __headers = {
+def get_web_page(url):
+    headers = {
         'User-Agent':
-        'Mozilla/5.0 (Windows NT 6.1) Chrome/44.0.2403.157 Safari/537.36'}
-
-    def __init__(self, url=""):
-        self.response = requests.get(url, headers=self.__headers)
-
-    def status_code(self):
-        return self.response.status_code
-
-    def text(self):
-        return self.response.text
+        'Mozilla/5.0 (Windows NT 6.1) Chrome/44.0.2403.157 Safari/537.36'
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None
 
 
-soup = BeautifulSoup(Response(url).text(), 'html5lib')
+def main():
+    soup = get_web_page(url)
+
+
+if __name__ == '__main__':
+    main()
--- function_tests.py
+++ function_tests.py
@@ -12,23 +12,21 @@ import re
 
 
 # 把想要抓取的小說，給於基本資料
+
 # 小說標題
 print(downloader.soup.find('h1', {'id': re.compile('thread')}).text)
 # next url
 print(downloader.soup.find('a', {'class': 'nxt'})['href'])
-#
-test = [item for item in downloader.soup.find_all('div', {'id': re.compile('post_')})]
-print(test[0].text.encode('utf-8').strip())
+# 文章內容
+test = [item for item in downloader.soup.find_all('td', {'id': re.compile('postmessage_')})]
+
+for item in test:
+    print(item.text.strip())
+
 """
     傳送請求，存取小說在指定的位置
         傳送請求，獲得需要的小說
 """
-        # 抓取下一頁 url
-
-
-        # 抓取文章
-
-
 
         # 內容需要修改的部分進行處理
 
