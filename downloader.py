1a63278f85c4956f8741d95888b2dbcb297ac272
hadesshark <hades_shark@hotmail.com>
2017-06-09 15:17:04 +0800

    collect 內的 downloader 有點奇怪

--- downloader.py
+++ downloader.py
@@ -2,39 +2,8 @@
 import requests
 import re
 import sys
-import time
 from lxml import etree
 import json
-
-def get_web_page(url):
-    headers = {
-        'User-Agent':
-        'Mozilla/5.0 (Windows NT 6.1) Chrome/44.0.2403.157 Safari/537.36'
-    }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return etree.HTML(response.text.encode('utf-8'))
-    else:
-        return None
-
-def get_page_chapters(url):
-    xpath_content = u"//td[@class='t_f']//text()"
-    etree_page = get_web_page(url).xpath(xpath_content)
-    content = ''
-    for item in etree_page:
-        content += item + '\n\n'
-    return content
-
-# 全部頁數
-def get_all_page_num(url):
-    xpath_all_num = u"//div[@class='pg']/a[@class='last']//text()"
-    etree_page = get_web_page(url)
-    all_num = etree_page.xpath(xpath_all_num)[0]
-
-    print(all_num.split(' ')[-1])
-
-def show_download_info(url):
-    sys.stdout.write("\rurl: {0}".format(url))
 
 class SettingInfo(object):
     def __init__(self):
@@ -54,19 +23,43 @@ class SettingInfo(object):
         print(self.title)
 
 
-class downloader(object):
+class Downloader(object):
     __xpath_content = u"//td[@class='t_f']//text()"
     __xpath_next_url = u"//div[@class='pg']/a[@class='nxt']/@href"
+    __xpath_all_num = u"//div[@class='pg']/a[@class='last']//text()"
+    __headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows NT 6.1) Chrome/44.0.2403.157 Safari/537.36'
+    }
 
-    def chapter_list(self, url):
-        return get_web_page(url).xpath(self.__xpath_content)
+    def __init__(self, url):
+        self.url = url
 
-    def get_next_url(self, url):
+    def chapter_list(self):
+        return self.page_analysis(self.__xpath_content)
+
+    def get_next_url(self):
         try:
-            etree_page = get_web_page(url)
-            return etree_page.xpath(self.__xpath_next_url)[0]
+            return self.page_analysis(self.__xpath_next_url)[0]
         except:
             return None
+
+    def get_all_page_num(self):
+        all_num = self.page_analysis(self.__xpath_all_num)[0]
+        print(all_num)
+
+    def page_analysis(self, path):
+        return self.get_web_page().xpath(path)
+
+    def get_web_page(self):
+        response = requests.get(self.url, headers=self.__headers)
+        if response.status_code == 200:
+            return etree.HTML(response.text.encode('utf-8'))
+        else:
+            return None
+
+    def show_now_url(self):
+        sys.stdout.write("\rurl: {0}".format(self.url))
 
 
 class Chapter(object):
@@ -75,17 +68,22 @@ class Chapter(object):
         self.content = ''
 
     def collect(self):
-        while self.url:
-            show_download_info(self.url)
+        while self.have_url():
+            downloader = Downloader(self.url)
+            downloader.show_now_url()
 
-            self.get_page_chapters()
-            self.url = downloader().get_next_url(self.url)
+            for item in downloader.chapter_list():
+                self.content += item + '\n\n'
+
+            self.set_url(downloader.get_next_url())
 
         return self.content
 
-    def get_page_chapters(self):
-        for item in downloader().chapter_list(self.url):
-            self.content += item + '\n\n'
+    def have_url(self):
+        return True if self.url else False
+
+    def set_url(self, url):
+        self.url = url
 
 
 class Novel(object):
@@ -101,10 +99,15 @@ class Novel(object):
         self.info.show_title()
 
 def main():
+    """
+    novel save
+    -> chpater collect
+    -> downloader chapter list
+
+    """
     novel = Novel()
     novel.show_title()
     novel.save()
-
 
 if __name__ == '__main__':
     main()
