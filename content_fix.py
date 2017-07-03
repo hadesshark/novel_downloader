51fa7224fb3068b3b80adb8fb00db6e5cdc86c4b
hadesshark <hades_shark@hotmail.com>
2017-07-03 11:55:07 +0800

    feat: 也是增加 author 所造成的修改

--- content_fix.py
+++ content_fix.py
@@ -9,58 +9,79 @@ from tqdm import *
 class JsonFile(object):
     def __init__(self):
         with open("setting.json", encoding="utf-8") as json_file:
-            self.json_data = json.load(json_file)[0].get('title')
-            self.title = self.name()
+            self.json_data = json.load(json_file)[0]
 
-    def name(self):
-        return self.json_data + ".txt"
+    def get_title(self):
+        return self.json_data.get('title')
+
+    def get_url(self):
+        return self.json_data.get('url')
+
+    def get_author(self):
+        return self.json_data.get('author')
 
     def __str__(self):
-        return ("{0} size: {1}".format(
-            self.json_data, os.path.getsize(self.name())))
+        return ("{0} 作者：{1}.txt").format(self.get_title(), self.get_author())
 
 
 def show_information(file_name, system_name):
     print("{0} size: {1}".format(file_name, os.path.getsize(system_name)))
 
 
+class Content(object):
+
+    def __init__(self):
+        self.txt_space = r"./txt_file//"
+        self.before_update = self.txt_space + "temp.txt"
+        self.after_update = self.txt_space + "temp2.txt"
+
+        self.json_file = JsonFile()
+        self.finish_version = self.txt_space + self.json_file.__str__()
+        self.file_name = self.json_file.json_data
+
+        self.content = "".encode('utf-8')
+
+    def txt_space_fix(self):
+        if not(os.path.exists(self.txt_space)):
+            os.mkdir(self.txt_space)
+
+        if os.path.isfile(self.before_update):
+            os.remove(self.before_update)
+
+        if os.path.isfile(self.after_update):
+            os.remove(self.after_update)
+
+    def check(self):
+        file = open(self.before_update, "rb")
+
+        for item in tqdm(file.readlines()):
+            if len(item) == 1:
+                continue
+            elif "  ".encode('utf-8') in item:  # 多空格行
+                continue
+            elif "本帖最後由".encode('utf-8') in item:
+                continue
+            else:
+                self.content += item
+        file.close()
+
+    def update(self):
+        self.txt_space_fix()
+
+        shutil.copy(self.finish_version, self.before_update)
+        show_information(self.file_name, self.finish_version)
+
+        self.check()
+
+        with open(self.after_update, "wb") as file:
+            file.write(self.content)
+
+        shutil.copy(self.after_update, self.finish_version)
+        show_information(self.file_name, self.finish_version)
+
+
 def main():
-
-    txt_space = r"./txt_file//"
-    if os.path.exists(txt_space):
-        os.mkdir(txt_space)
-
-    if os.path.isfile(txt_space + "temp.txt"):
-        os.remove(txt_space + "temp.txt")
-
-    if os.path.isfile(txt_space + "temp2.txt"):
-        os.remove(txt_space + "temp2.txt")
-
-    json_file = JsonFile()
-    shutil.copy(txt_space + json_file.title, txt_space + "temp.txt")
-    show_information(json_file.json_data, txt_space + json_file.title)
-
-
-    file = open(txt_space + "temp.txt", "rb")
-
-    content = "".encode('utf-8')
-    for item in tqdm(file.readlines()):
-        if len(item) == 1:
-            continue
-        elif "  ".encode('utf-8') in item:  # 多空格行
-            continue
-        elif "本帖最後由".encode('utf-8') in item:
-            continue
-        else:
-            content += item
-    file.close()
-
-    with open(txt_space + "temp2.txt", "wb") as file:
-        file.write(content)
-
-    shutil.copy(txt_space + "temp2.txt", txt_space + json_file.title)
-    show_information(json_file.json_data, txt_space + json_file.title)
-
+    Content().update()
 
 if __name__ == '__main__':
     main()
