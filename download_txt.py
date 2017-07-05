f81e9cc89a24b2d134b02adf55d5ac3226518943
hadesshark <hades_shark@hotmail.com>
2017-07-05 09:44:29 +0800

    refactor: 把之前可以執行的程式挪到 test 內

--- download_txt.py
+++ download_txt.py
@@ -8,23 +8,6 @@ from downloader import Downloader
 from opencc import OpenCC
 from content_fix import Content
 
-# url = 'http://www.xiaoqiangxs.com/modules/article/txtarticle.php?id=7072'
-#
-# response = requests.get(url)
-# response.encoding = 'gb18030'
-#
-# with open("test.txt", mode='w') as file:
-#     file.write(response.text)
-
-# file = open("test.txt", mode='r')
-#
-# opencc = OpenCC('s2tw')
-# content = ''
-# for item in file.readlines():
-#     content += opencc.convert(item)
-#
-# with open("test2.txt", mode='w') as file:
-#     file.write(content)
 
 class Downloader(Downloader):
 
--- test/function_tests.py
+++ test/function_tests.py
@@ -23,3 +23,23 @@ del_i_label()
 
 with open('temp.txt', 'w', encoding='utf-8') as f:
     f.write(temp_page.text)
+
+# from opencc import OpenCC
+# # 亂碼轉簡體再轉繁體
+# url = 'http://www.xiaoqiangxs.com/modules/article/txtarticle.php?id=7072'
+#
+# response = requests.get(url)
+# response.encoding = 'gb18030'
+#
+# with open("test.txt", mode='w') as file:
+#     file.write(response.text)
+#
+# file = open("test.txt", mode='r')
+#
+# opencc = OpenCC('s2tw')
+# content = ''
+# for item in file.readlines():
+#     content += opencc.convert(item)
+#
+# with open("test2.txt", mode='w') as file:
+#     file.write(content)
